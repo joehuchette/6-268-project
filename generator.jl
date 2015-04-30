640d4014af8765d52aee390aea32bce9337a4d49
Joey Huchette <joehuchette@gmail.com>
2015-04-30 12:25:53 -0400

    add population assignment function

--- generator.jl
+++ generator.jl
@@ -1,27 +1,30 @@
 using LightGraphs, Distributions, GraphLayout, Compose
 
+function linear_to_grid(k, N)
+	i = mod(k-1, 2N+1) - N
+	j = div(k-1, 2N+1) - N
+	return (i,j)
+end
+
+grid_to_linear(i, j, N) = (j+N) * (2N+1) + (i+N) + 1
+
+
 # Creates a 2D lattice on [-N,N] × [-N,N]. Adds N^α random shortcuts, where the
 # probability that the edge has a particular length is proportional to the distance
 # to the -β-th power.
 function graph_generator(N, α, β)
 
-	function linear_to_grid(k)
-		i = mod(k-1, 2N+1) - N
-		j = div(k-1, 2N+1) - N
-		return (i,j)
-	end
-
-	grid_to_linear(i, j) = (j+N) * (2N+1) + (i+N) + 1	
+	g2l(i, j) = grid_to_linear(i, j, N)
 
 	# step 1: create the underlying lattice
 	gr = Graph((2N+1)^2)
 	for i in -N:(N-1), j in -N:(N-1)
-		add_edge!(gr, grid_to_linear(i,j), grid_to_linear(i+1,j))
-		add_edge!(gr, grid_to_linear(i,j), grid_to_linear(i,j+1))
+		add_edge!(gr, g2l(i,j), g2l(i+1,j))
+		add_edge!(gr, g2l(i,j), g2l(i,j+1))
 	end
 	for i in -N:(N-1)
-		add_edge!(gr, grid_to_linear(N,i), grid_to_linear(N,i+1))
-		add_edge!(gr, grid_to_linear(i,N), grid_to_linear(i+1,N))
+		add_edge!(gr, g2l(N,i), g2l(N,i+1))
+		add_edge!(gr, g2l(i,N), g2l(i+1,N))
 	end
 
 	# step 2: add shortcuts
@@ -43,12 +46,12 @@ function graph_generator(N, α, β)
 		# accpetance rejection for other endpoint, break on success
 		while true
 			k, l = rand(du), rand(du)
-			if (k,l) == (i,j) || has_edge(gr, grid_to_linear(i,j), grid_to_linear(k,l))
+			if (k,l) == (i,j) || has_edge(gr, g2l(i,j), g2l(k,l))
 				continue
 			end
 			dβ = 1 / (abs(i-k) + abs(j-l))^β
 			if rand() < dβ / c
-				add_edge!(gr, grid_to_linear(i,j), grid_to_linear(k,l))
+				add_edge!(gr, g2l(i,j), g2l(k,l))
 				break
 			end
 		end
@@ -61,4 +64,47 @@ function plot_graph(gr::Graph)
 	am = full(adjacency_matrix(gr))
 	loc_x, loc_y = layout_spring_adj(am)
 	draw_layout_adj(am, loc_x, loc_y, filename="lattice-with-jumps.svg")
+end
+
+function plot_graph(gr::Graph, weights)
+	am = full(adjacency_matrix(gr))
+	loc_x, loc_y = layout_spring_adj(am)
+	draw_layout_adj(am, loc_x, loc_y, filename="lattice-with-jumps.svg", labels=weights)
+end
+
+# function distances_to_origin(gr)
+# 	V = length(vertices(gr))
+# 	N = convert(Int, (sqrt(V) - 1) / 2)
+# 	dij = dijkstra_shortest_paths(gr, grid_to_linear(0,0,N))
+# 	return dij.dists
+# end
+
+function assign_population(gr, M, γ, δ)
+	V = length(vertices(gr))
+	N = convert(Int, (sqrt(V) - 1) / 2)
+	dij = dijkstra_shortest_paths(gr, grid_to_linear(0,0,N))
+	dist = dij.dists
+	q = 1 ./ dist^γ
+	# Manually say that no-one lives in the center
+	q[grid_to_linear(0,0,N)] = 0.0
+	qtot = sum(q)
+	pop = zeros(Int, V)
+
+	du = DiscreteUniform(1,V)
+	for _ in 1:M
+		i = 0
+		while true
+			i = rand(du)
+			r = rand()
+			if r < q[i] / qtot
+				break
+			end
+		end
+		@show linear_to_grid(i,N)
+		pop[i] += 1
+		qtot -= q[i]
+		q[i] = 1 / (dist[i]^γ * (pop[i]+1)^δ)
+		qtot += q[i]
+	end
+	return pop
 end
