e40470dbc67bbfd0a2a6af23b3f0d757992b1cc4
evanfields <ejf12009@mymail.pomona.edu>
2015-05-04 13:55:30 -0400

    average density by distance

--- experimentCode.jl
+++ experimentCode.jl
@@ -16,3 +16,21 @@ function test_params(N::Int, m::Int, alpha, beta, gamma, delta, reps::Int)
 	end
 	return (gr, pops)
 end
+
+# given a graph and list of populations
+# find the average density by graph distance to origin
+function get_avg_density_by_dist(gr,pops)
+	V = length(vertices(gr))
+	N = convert(Int, (sqrt(V) - 1) / 2)
+	dij = dijkstra_shortest_paths(gr, grid_to_linear(0,0,N))
+	dists = dij.dists
+	mean_pops = mean(pops,1) # mean for each vertex
+	maxDist = 2*N
+	pointsAtDist = zeros(maxDist)
+	peopleAtDist = zeros(maxDist)
+	for i = 1:V
+		peopleAtDist[dists[i] += mean_pops[i]
+		pointsAtDist[dists[i]] += 1
+	end
+	return peopleAtDist ./ pointsAtDist
+end
