c6561aedf26ecdad1b4602023c17a970f78f2011
Joey Huchette <joehuchette@gmail.com>
2015-05-11 13:40:53 -0400

    add overcrowding penalization to model

--- optimal_network.jl
+++ optimal_network.jl
@@ -1,21 +1,22 @@
 using JuMP
 
-function optimal_allocation(M, N, P, β, γ)
+function optimal_allocation(M, N, P, β, γ, ω)
 
 	model = Model()
 
 	@defVar(model, 0 <= v[-N:1:N, -N:1:N] <= M, Int)
 	@defVar(model, aux[keys(P)])
 	@addConstraint(model, v[0,0] == 0)
-	
+
 	#total population constraint
 	@addConstraint(model, sum{v[i,j], i=-N:N, j=-N:N} == M)
-	
+
 	for k in keys(P)
 		@addConstraint(model, aux[k] == sum{v[i,j], (i,j) = P[k]})
 	end
 
-	@setObjective(model, Min, sum{β*aux[k] + γ*aux[k]^2, k = keys(P)})
+	@setObjective(model, Min, sum{β*aux[k] + γ*aux[k]^2, k = keys(P)}
+							+ sum{ω*v[i,j]^2, i in -N:N, j in -N:N})
 
 	solve(model)
 	return getValue(v)
